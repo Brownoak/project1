2b33d80379535530aa7617207703cece672a5f46
@Brownoak <helanii93@gmail.com>
2023-03-15 23:13:16 +0300

    removing unnecessary import

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import "./index.css";
 import HomePage from "./scenes/homePage";
 import HomePageTwo from "./scenes/homePageTwo";
 import LoginPage from "./scenes/loginPage";
-import NavBar from "./scenes/navBar";
 import RegisterPage from "./scenes/registerPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <NavBar/> */}
       <Routes>
         <Route path="/" element={<LoginPage />}></Route>
         <Route path="register" element={<RegisterPage />}></Route>
