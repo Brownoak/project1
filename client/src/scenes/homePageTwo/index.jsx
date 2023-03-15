c666481054946b4ca822d358635efc3a7718e29c
@Brownoak <helanii93@gmail.com>
2023-03-15 23:02:33 +0300

    successful log in page

---
+++ client/src/scenes/homePageTwo/index.jsx
@@ -0,0 +1,12 @@
+
+const HomePageTwo=()=>{
+    return(
+        <div className="pt-14">
+            <p className="flex justify-center align-center text-2xl">Successful Login</p>
+            <p className="flex justify-center align-center text-xl">Welcome </p>
+          
+        </div>
+    )
+}
+
+export default HomePageTwo
